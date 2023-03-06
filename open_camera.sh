b83e5c67c42b31ffda8006701dca2512106f2c26
Bohemia_vicky <68543155+bohemianskode@users.noreply.github.com>
2023-03-07 02:52:27 +0700

    Add files via upload

---
+++ open_camera.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+adb shell am start -a android.media.action.IMAGE_CAPTURE
+
+adb shell am start -a android.media.action.IMAGE_CAPTURE
+``
+
+adb shell am start -a android.media.action.IMAGE_CAPTURE
+
+adb shell am start -a android.media.action.IMAGE
+
+adb shell am start -a android.media.action
+
+adb shell am start -a android
+
+adb shell am start -a
+
+adb shell am start
+
+adb shell am
+
+adb shell
+
+adb
